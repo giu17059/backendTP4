120c08a4435879e4635f566ca15ee57d6b8d9406
lcastellano <castellanoluciaveronica@gmail.com>
2024-11-13 23:43:53 -0300

    consultas Autor

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,4 +1,4 @@
-const Author = require('../models/author'); 
+const Author = require('../models/author');  
 
     exports.createAuthor = async (req, res) => {
         try {
@@ -14,7 +14,7 @@ const Author = require('../models/author');
             res.status(500).json({message: 'Error al crear Autor'})
         }
     },
-    exports.getAuthor = async (req, res) => {
+    exports.getAuthors = async (req, res) => {
         try{
             const authors = await Author.findAll();
 
@@ -24,5 +24,39 @@ const Author = require('../models/author');
             res.status(500).json({message: 'Error al obtener autores'})
         }
     };
-
+    exports.getAuthor = async (req, res) => {
+        try {
+            const author = await Author.findByPk(req.params.id);
+            if (!author) {
+                return res.status(404).json({ error: 'Autor no encontrado' });
+            }
+            res.json(author);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    };
+    exports.updateAuthor = async (req, res) => {
+        try {
+            const author = await Author.findByPk(req.params.id);
+            if (!author) {
+                return res.status(404).json({ error: 'Autor no encontrado' });
+            }
+            const updatedAuthor = await author.update(req.body);
+            res.json(updatedAuthor);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    };
+    exports.deleteAuthor = async (req, res) => {
+        try {
+            const author = await Author.findByPk(req.params.id);
+            if (!author) {
+                return res.status(404).json({ error: 'Autor no encontrado' });
+            }
+            await author.destroy();
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    };
 
--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createAuthor, getAuthor } = require('../controllers/authorControllers');
+const { createAuthor, getAuthors, getAuthor, deleteAuthor, updateAuthor} = require('../controllers/authorControllers');
 
+//Ruta para crear autores
 router.post('/', createAuthor);
-router.get('/', getAuthor);
+
+//Ruta para obtener todos los autores
+router.get('/', getAuthors);
+
+// Ruta para obtener un autor por ID
+router.get('/:id', getAuthor);
+
+// Ruta para actualizar un autor
+router.put('/:id', updateAuthor);
+
+// Ruta para eliminar un autor
+router.delete('/:id', deleteAuthor);
 
 module.exports = router;
